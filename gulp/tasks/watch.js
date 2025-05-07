f1a074e717720bbd410931ab7f75f0efbaec29d9
Sahethi <dgsahethi@gmail.com>
2025-05-07 20:25:08 +0530

    changed message

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,29 +1,32 @@
 var gulp = require("gulp"),
-    watch = require("gulp-watch"),
-    browserSync = require("browser-sync").create();
-    
-gulp.task("watch", function(){
-    browserSync.init({
-        notify: false,
-        server:{
-           baseDir: "app"
-       }
-    });
-    watch("./app/index.html", function(){
-        browserSync.reload();
-    });
-    
-    watch("./app/assets/styles/**/*.css", gulp.series('css', 'cssInject'));
-    
-    //./app/assets/scripts/**/*.js any subfolder(**) which contains any .js files(*)
-    watch("./app/assets/scripts/**/*.js", gulp.series('scripts', 'reloadBrowser'));
+  watch = require("gulp-watch"),
+  browserSync = require("browser-sync").create();
+
+gulp.task("watch", function () {
+  browserSync.init({
+    notify: false,
+    open: false,
+    server: {
+      baseDir: "app",
+    },
+  });
+  watch("./app/index.html", function () {
+    browserSync.reload();
+  });
+
+  watch("./app/assets/styles/**/*.css", gulp.series("css", "cssInject"));
+
+  //./app/assets/scripts/**/*.js any subfolder(**) which contains any .js files(*)
+  watch(
+    "./app/assets/scripts/**/*.js",
+    gulp.series("scripts", "reloadBrowser")
+  );
 });
 
-gulp.task("cssInject", function(){
-    return gulp.src("./app/css/style.css")
-    .pipe(browserSync.stream());
+gulp.task("cssInject", function () {
+  return gulp.src("./app/css/style.css").pipe(browserSync.stream());
 });
 
-gulp.task("reloadBrowser", function(){
-   browserSync.reload(); 
+gulp.task("reloadBrowser", function () {
+  browserSync.reload();
 });
